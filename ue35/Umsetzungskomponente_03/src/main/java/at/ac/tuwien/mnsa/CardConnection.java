710877e067410d3545f8cf9ac9bb0f78cbf4d32c
Stefan Gamerith <steff7@gmx.com>
2013-12-05 16:50:51 +0100

    First alpha version

---
+++ ue35/Umsetzungskomponente_03/src/main/java/at/ac/tuwien/mnsa/CardConnection.java
@@ -0,0 +1,34 @@
+package at.ac.tuwien.mnsa;
+
+import javax.microedition.contactless.ContactlessException;
+import javax.microedition.contactless.TargetProperties;
+import javax.microedition.contactless.sc.ISO14443Connection;
+import javax.microedition.io.Connector;
+import java.io.IOException;
+
+public class CardConnection {
+
+	private final ISO14443Connection connection;
+
+	public CardConnection(ISO14443Connection connection) {
+		this.connection = connection;
+	}
+
+	public static CardConnection open(TargetProperties targetProperties) throws IOException {
+		String url = targetProperties.getUrl(targetProperties.getConnectionNames()[0]);
+		ISO14443Connection connection = (ISO14443Connection) Connector.open(url);
+		return new CardConnection(connection);
+	}
+
+	public byte[] exchangeData(byte[] request) throws IOException {
+		try {
+			return connection.exchangeData(request);
+		} catch (ContactlessException e) {
+			throw new IOException(e);
+		}
+	}
+
+	public void close() throws IOException {
+		connection.close();
+	}
+}
---
+++ ue35/Umsetzungskomponente_03/src/main/java/at/ac/tuwien/mnsa/Midlet.java
@@ -0,0 +1,42 @@
+package at.ac.tuwien.mnsa;
+
+import javax.microedition.contactless.ContactlessException;
+import javax.microedition.contactless.DiscoveryManager;
+import javax.microedition.contactless.TargetType;
+import javax.microedition.lcdui.*;
+import javax.microedition.midlet.MIDlet;
+import javax.microedition.midlet.MIDletStateChangeException;
+
+public class Midlet extends MIDlet {
+
+	private Form form;
+
+	protected void startApp() throws MIDletStateChangeException {
+		form = new Form("Main Form");
+		Display.getDisplay(this).setCurrent(form);
+
+		final Command exitCommand = new Command("Exit", Command.EXIT, 1);
+		form.addCommand(exitCommand);
+		form.setCommandListener(new CommandListener() {
+			public void commandAction(Command command, Displayable displayable) {
+				if (command == exitCommand) {
+					notifyDestroyed();
+				}
+			}
+		});
+
+		try {
+			CardConnector connector = new CardConnector();
+			DiscoveryManager discoveryManager = DiscoveryManager.getInstance();
+			discoveryManager.addTargetListener(connector, TargetType.ISO14443_CARD);
+		} catch (ContactlessException e) {
+
+		}
+	}
+
+	protected void pauseApp() {
+	}
+
+	protected void destroyApp(boolean b) throws MIDletStateChangeException {
+	}
+}
